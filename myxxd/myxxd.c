56bdce77a9de1be6ba43b3a97e6fb029fdadbea3
kyle <kingkellingson@gmail.com>
2021-09-18 19:07:15 -0600

    2

--- myxxd/myxxd.c
+++ myxxd/myxxd.c
@@ -119,15 +119,17 @@ void printDataAsBits (unsigned char *data, size_t size) {
         //printf("%d", data[i]); 
         for (int j = 0; j < 8; ++j) { //for each of the 8 bits
           if (byte % 2 == 1) {
-            eightBit[j] = 1;
+            printf("1");
+            //eightBit[j] = 1;
             /* store a 1 for the bit */
           } else {
-            eightBit[j] = 0;
+            printf("0");
+            //eightBit[j] = 0;
             /* store a 0 for the bit */
           }
           byte = byte / 2;
         }
-        printf("%s", eightBit);
+        //printf("%s", eightBit);
         //printf("%04x", data[i]);
       }
       else {
